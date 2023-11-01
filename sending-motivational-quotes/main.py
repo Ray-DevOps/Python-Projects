5d99c6fb907f15607c6880cfe1bc84aa3afd88b2
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-11-01 07:14:04 -0400

    Create main.py

---
+++ sending-motivational-quotes/main.py
@@ -0,0 +1,38 @@
+#                 DESCRIPTION
+#           ------------------------
+
+# In this project, we check whther the day of the week is Wednesday. If yes, we send a
+# randon motivational message to a friend from a list of messages in the txt file
+
+
+import smtplib
+import random
+my_email = "anyaray2016@gmail.com"
+my_password = "wsoiuzkmtuawaiqm"                
+
+quotes_list = [ ]
+with open('quotes.txt') as quotes:
+    lines = quotes.readlines()
+    for line in lines:
+        line = line.strip()
+        quotes_list.append(line)
+
+message = random.choice(quotes_list)
+
+import datetime as dt
+
+time_now = dt.datetime.now()
+
+year = time_now.year
+weekday = time_now.weekday()
+date_of_birth = dt.datetime(year=1995, month=12, day=25)
+
+if weekday == 2:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="anyaraymond@yahoo.com",
+            msg=f"Subject: Happy New Day\n\n{message}"
+        )
